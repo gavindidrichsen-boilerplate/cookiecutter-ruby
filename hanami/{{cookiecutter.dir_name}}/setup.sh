19f308e1978b4e8b1c69b11b6e354dcfe022aeb3
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-15 08:58:32 +0100

    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- hanami/{{cookiecutter.dir_name}}/setup.sh
+++ hanami/{{cookiecutter.dir_name}}/setup.sh
@@ -6,3 +6,7 @@ bundle install
 
 if [[ ! -d {{cookiecutter.hanami_application}} ]]; then bundle exec hanami new {{cookiecutter.hanami_application}}; fi
 if [[ ! -f {{cookiecutter.hanami_application}}/Guardfile ]]; then cp .Guardfile {{cookiecutter.hanami_application}}/Guardfile; fi
+
+# ensure rspec result file exists to prevent intermittent errors from occurring
+mkdir -p {{cookiecutter.hanami_application}}/tmp
+touch {{cookiecutter.hanami_application}}/tmp/rspec_guard_result
